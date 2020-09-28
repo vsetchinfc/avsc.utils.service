8071b65bcff8e439416ac67a9d367ea9a812ef38
Vladislav Setchin <vlad@setchinfc.com.au>
2020-09-28 13:37:01 +1000

    Added Utility functions for Service

---
+++ avsc.utils.service/Enums/HostAction.cs
@@ -0,0 +1,13 @@
+namespace AVSC.Utils.Service.Enums
+{
+    public enum HostAction
+    {
+        ShowUsage,
+        InstallWinService,
+        UninstallWinService,
+        RunWinService,
+        WinServiceStop,
+        RunConsole,
+        RunLinuxDaemon
+    }
+}
---
+++ avsc.utils.service/Interfaces/IHostAction.cs
@@ -0,0 +1,9 @@
+using AVSC.Utils.Service.Enums;
+
+namespace AVSC.Utils.Service.Interfaces
+{
+    public interface IHostAction
+    {
+        HostAction GetHostAction();
+    }
+}
